36aeeb58ed561ba1e76e73035d8edf1cf820cb9b
Lucy Massie <106594174+LJMas@users.noreply.github.com>
2022-06-29 10:39:05 +0100

    Write tests and code for players taking turns (#3)
    I had a play and got it to pass. Changes required in tic_tac_toe.rb:
    
    -included `initialize` method that creates instance variables for `@board` and `@players`
    -board and players methods now only contain their respective instance variable eg `@board`
    
    Changes required in tic_tac_toe_spec.rb:
    
    -where at the beginning we created an instance variable `@tic_tac_toe`, I took it out of its `before .. do .. end` and made it just a normal local variable `tic_tac_toe`, and changed this on each test.

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,58 +1,83 @@
 require_relative "../tic_tac_toe.rb"
 
 describe "Tic Tac Toe" do
-    before do
-        @tic_tac_toe = TicTacToe.new
-    end
+
+    tic_tac_toe = TicTacToe.new
 
     context "tests for the board" do
         it "is an array" do
-            expect(@tic_tac_toe.board.class).to eq Array
+            expect(tic_tac_toe.board.class).to eq Array
         end
         
         it "has 9 elements in it" do
-            expect(@tic_tac_toe.board.length).to eq 9
+            expect(tic_tac_toe.board.length).to eq 9
         end
 
         it "is empty" do
-            expect(@tic_tac_toe.board).to all eq nil
+            expect(tic_tac_toe.board).to all eq nil
         end
     end
 
     context "test for players" do
         it "exists" do
-            expect(@tic_tac_toe.players).to_not be nil
+            expect(tic_tac_toe.players).to_not be nil
         end
 
         it "has 2 players" do
-            expect(@tic_tac_toe.players.length).to eq 2
+            expect(tic_tac_toe.players.length).to eq 2
         end
 
         it "contains hashes" do
-            expect(@tic_tac_toe.players).to all be_a(Hash)
+            expect(tic_tac_toe.players).to all be_a(Hash)
         end
 
         it "has a key with the value 'X' or 'O'" do
-            expect(@tic_tac_toe.players[0]["player"]).to eq "X"
-            expect(@tic_tac_toe.players[1]["player"]).to eq "O"
+            expect(tic_tac_toe.players[0]["player"]).to eq "X"
+            expect(tic_tac_toe.players[1]["player"]).to eq "O"
         end
 
         it "is X's turn" do
-            expect(@tic_tac_toe.players[0]["my_turn"]).to eq true
+            expect(tic_tac_toe.players[0]["my_turn"]).to eq true
         end
 
         it "is not O's turn" do
-            expect(@tic_tac_toe.players[1]["my_turn"]).to eq false
+            expect(tic_tac_toe.players[1]["my_turn"]).to eq false
         end
     end
 
     context "winning combinations" do
         it "has 8" do
-            expect(@tic_tac_toe.winning_combinations.length).to eq 8
+            expect(tic_tac_toe.winning_combinations.length).to eq 8
         end
 
         it "has 3 coordinates" do
-            expect(@tic_tac_toe.winning_combinations).to all have_attributes(size: 3)
+            expect(tic_tac_toe.winning_combinations).to all have_attributes(size: 3)
         end
     end
+
+    context "taking a turn" do
+        it "receives coordinates" do
+            expect(tic_tac_toe.turn).to eq 4
+        end
+        it "checks something is not already in the specified square" do
+            expect(tic_tac_toe.is_square_free).to be true
+        end
+
+        it "checks whose turn it is" do
+            expect(tic_tac_toe.check_turn).to eq "X" 
+        end
+
+        it "updates the board" do
+            expect(tic_tac_toe.update_board(4)).to eq [nil,nil,nil,nil,"X",nil,nil,nil,nil]
+        end
+
+        it "updates the user's turn" do
+            expect(tic_tac_toe.update_turn).to eq [{"my_turn"=>false, "player"=>"X"}, {"my_turn"=>true, "player"=>"O"}]
+        end
+
+        it "returns an error message if square is occupied" do
+            expect(tic_tac_toe.is_square_free).to eq "Please choose another square"
+        end
+    end
+
 end
