fde3df60e9a2533348f52af6e3f2ba0c7de8fec4
LJMas <lucy.massie@madetech.com>
2022-06-29 10:29:24 +0100

    Write tests and code for players taking turns

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,12 +1,17 @@
 class TicTacToe
+    def initialize
+        @board = Array.new(9)
+        @players = [
+            { "player" => "X", "my_turn" => true  } ,
+            { "player" => "O", "my_turn" => false }
+          ]
+    end
     def board
-        Array.new(9)
+        @board
     end
     
     def players
-        [ { "player" => "X", "my_turn" => true  } ,
-          { "player" => "O", "my_turn" => false}
-        ]
+        @players
     end
 
     def winning_combinations
@@ -20,6 +25,26 @@ class TicTacToe
             [3, 4 ,5],
             [6, 7 ,8],
         ]
+    end
+
+    def turn 
+        4
+    end
+
+    def is_square_free
+        board[turn] == nil ? true : "Please choose another square"
+    end
+
+    def check_turn
+        players.select { | player | return player["player"] if player["my_turn"] == true }
+    end
+
+    def update_board(coord)
+        @board = board.each_with_index.map{ | value, index | index == coord ? check_turn : value }
+    end
+
+    def update_turn
+        @players = players.each{ | player | player["my_turn"] == true ?  player["my_turn"] = false : player["my_turn"] = true }
     end
 end
 
