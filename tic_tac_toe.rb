954b184d25f7e88ce5e96495e0e36b81ba9b156b
Lucy Massie <106594174+LJMas@users.noreply.github.com>
2022-07-06 12:27:58 +0100

    Put components of the game together (#4)
    * Putting components of the game together
    
    * Trying to make loop work ... :(
    
    * Finally put game together and everything works! :)

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'stringio'
+
 class TicTacToe
     def initialize
         @board = Array.new(9)
@@ -6,8 +8,16 @@ class TicTacToe
             { "player" => "O", "my_turn" => false }
           ]
     end
+
     def board
         @board
+    end
+
+    def display_board(board)
+        show_board = board.map { |element| element == nil ? " " : element}
+        
+        puts "\n", " #{show_board[0]} | #{show_board[1]} | #{show_board[2]} \n-----------\n #{show_board[3]} | #{show_board[4]} | #{show_board[5]} \n-----------\n #{show_board[6]} | #{show_board[7]} | #{show_board[8]} "
+        " #{show_board[0]} | #{show_board[1]} | #{show_board[2]} \n-----------\n #{show_board[3]} | #{show_board[4]} | #{show_board[5]} \n-----------\n #{show_board[6]} | #{show_board[7]} | #{show_board[8]} "
     end
     
     def players
@@ -28,7 +38,9 @@ class TicTacToe
     end
 
     def turn 
-        4
+        @turn = $stdin.gets&.chomp
+        @turn ? @turn = @turn.to_i : nil
+        @turn
     end
 
     def is_square_free
@@ -46,9 +58,61 @@ class TicTacToe
     def update_turn
         @players = players.each{ | player | player["my_turn"] == true ?  player["my_turn"] = false : player["my_turn"] = true }
     end
+
+    def check_winning_combo(board)
+        locations_of_current_player = board.each_index.select{|i| board[i] == check_turn}
+        winning_combinations.each do | combination |
+          return true if (combination & locations_of_current_player) == combination
+        end
+        return false
+    end
+
+    def announce_winner
+        "Player #{check_turn} wins!"
+    end
+
+    def full_board?
+        @board.count(nil) == 0 ? true : false
+    end
+
+    def announce_draw
+        "GAME OVER! It's a draw."
+    end
+
+
+   # check if the board is full or if there is a winning combo
+   # solid state of when the game the game ends
+   # otherwise do this loop:
+   # while
+   # update the board with the whose turn it is
+   # / where they have put their move
+   # check if there is a winning combo
+   # if so, announce there is a winner
+   # otherwise we update the turn
+
+    # rescue => exception
+        
+    # end
+    def play_game
+        while !full_board? do
+            
+            return @board if !turn
+
+            update_board(@turn)
+            display_board(@board)
+
+            if check_winning_combo(@board)
+                puts announce_winner
+                return announce_winner
+            end
+
+            update_turn
+        end
+        puts announce_draw
+        announce_draw
+    end
 end
 
 
-
-
-
+tic_tac_toe = TicTacToe.new
+tic_tac_toe.play_game
